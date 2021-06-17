ce964d7ae606fa87ca2d33c041637710c508f10e
sarmerer <sarmerer@gmail.com>
2021-06-18 02:58:28 +0500

    fixed cards going off screen on resize implemented cards snapping to screen sides

--- src/components/cards/CardGroup.vue
+++ src/components/cards/CardGroup.vue
@@ -7,16 +7,40 @@
   >
     <div class="container">
       <div class="controls">
-        <div><b-icon icon="x" @click="deleteGroup(group.id)"></b-icon></div>
         <div
-          v-if="cards.length > 1"
+          title="Move"
+          class="control"
+          @mousedown="dragStart"
+          :class="{ active: dragging }"
+        >
+          <b-icon icon="arrows-move"></b-icon>
+        </div>
+        <div
+          title="Order"
+          class="control"
+          v-if="cards.length > 1 && !showSnaps"
           @click="orderMode = !orderMode"
           :class="{ active: orderMode }"
         >
           <b-icon icon="arrow-left-right"></b-icon>
         </div>
-        <div @mousedown="dragStart" :class="{ active: dragging }">
-          <b-icon icon="arrows-move"></b-icon>
+        <div class="control" title="Delete" v-if="!showSnaps">
+          <b-icon icon="x" @click="deleteGroup(group.id)"></b-icon>
+        </div>
+        <div class="control" :class="{ active: showSnaps }">
+          <b-icon icon="grid" @click="showSnaps = !showSnaps"></b-icon>
+        </div>
+        <div class="control" :class="{ active: snap.top }" v-if="showSnaps">
+          <b-icon icon="align-top" @click="snapTo('top')"></b-icon>
+        </div>
+        <div class="control" :class="{ active: snap.right }" v-if="showSnaps">
+          <b-icon icon="align-end" @click="snapTo('right')"></b-icon>
+        </div>
+        <div class="control" :class="{ active: snap.bottom }" v-if="showSnaps">
+          <b-icon icon="align-bottom" @click="snapTo('bottom')"></b-icon>
+        </div>
+        <div class="control" :class="{ active: snap.left }" v-if="showSnaps">
+          <b-icon icon="align-start" @click="snapTo('left')"></b-icon>
         </div>
       </div>
 
@@ -79,16 +103,21 @@ export default {
     isTarget() {
       return this.draggingCard && this.targetGroup === this.group.id;
     },
+    snap() {
+      return this.group.snap || {};
+    },
+    isSnapped() {
+      return Object.values(this.snap).some((s) => s === true);
+    },
   },
   watch: {
+    snap: {
+      deep: true,
+      handler: "reposition",
+    },
     boundaries(bounds) {
       if (!this.oldBounds) this.oldBounds = bounds;
-      const xDiff = (bounds?.width || 0) - (this.oldBounds?.width || 0);
-      const yDiff = (bounds?.height || 0) - (this.oldBounds?.height || 0);
-      const newX = this.group.x + xDiff;
-      const newY = this.group.y + yDiff;
-      if (newX >= 0 && newX <= bounds.width) this.group.x = newX;
-      if (newY >= 0 && newY <= bounds.height) this.group.y = newY;
+      this.reposition();
       this.oldBounds = bounds;
     },
   },
@@ -98,6 +127,7 @@ export default {
       shift: { x: 0, y: 0 },
       oldBounds: null,
       orderMode: false,
+      showSnaps: false,
     };
   },
   mounted() {
@@ -126,16 +156,31 @@ export default {
     },
     dragStart(e) {
       this.dragging = true;
+      // this.resetSnap();
+      // this.showSnaps = false;
       const rect = this.$el.getBoundingClientRect();
       this.shift.x = e.clientX - rect.left;
       this.shift.y = e.clientY - rect.top;
     },
     drag(e) {
       if (!this.dragging) return;
+      const rect = this.$el.getBoundingClientRect();
       const x = e.clientX - this.shift.x;
       const y = e.clientY - this.shift.y;
-      if (x > 0) this.group.x = x;
-      if (y > 0) this.group.y = y;
+      if (
+        !this.snap.left &&
+        !this.snap.right &&
+        x > 0 &&
+        x + rect.width <= this.boundaries?.width
+      )
+        this.group.x = x;
+      if (
+        !this.snap.top &&
+        !this.snap.bottom &&
+        y > 0 &&
+        y + rect.height <= this.boundaries?.height
+      )
+        this.group.y = y;
     },
     dragStop() {
       this.dragging = false;
@@ -145,6 +190,46 @@ export default {
     },
     closePicker(e) {
       this.$emit("close-picker", e);
+    },
+    snapTo(side) {
+      if (this.group.snap === null || typeof this.group.snap !== "object") {
+        this.group.snap = {};
+      }
+      this.group.snap[side] = !this.group.snap[side];
+      this.group.snap[oppoziteSide(side)] = false;
+
+      function oppoziteSide(side) {
+        const oppozites = {
+          top: "bottom",
+          right: "left",
+          bottom: "top",
+          left: "right",
+        };
+        return oppozites[side];
+      }
+    },
+    resetSnap() {
+      Object.keys(this.group.snap).forEach((k) => (this.group.snap[k] = false));
+    },
+    reposition() {
+      const rect = this.$el.getBoundingClientRect();
+      const bounds = this.boundaries;
+
+      let newX, newY;
+      if (this.isSnapped) {
+        if (this.snap.top) newY = 85;
+        if (this.snap.right) newX = bounds.width - rect.width;
+        if (this.snap.bottom) newY = bounds.height - rect.height - 10;
+        if (this.snap.left) newX = 10;
+      } else {
+        const xDiff = (bounds?.width || 0) - (this.oldBounds?.width || 0);
+        const yDiff = (bounds?.height || 0) - (this.oldBounds?.height || 0);
+        newX = this.group.x + xDiff;
+        newY = this.group.y + yDiff;
+      }
+
+      if (newX >= 0 && newX + rect.width <= bounds.width) this.group.x = newX;
+      if (newY >= 0 && newY + rect.height <= bounds.height) this.group.y = newY;
     },
   },
 };
@@ -159,22 +244,35 @@ export default {
     .controls {
       position: absolute;
       top: -1rem;
-      right: 0;
+      left: 0;
       display: flex;
-      gap: 0.2rem;
-      border: 1px solid purple;
-      border-radius: 0.4rem;
+      // border: 1px solid purple;
+      // border-radius: 0.4rem;
       background-color: white;
       z-index: 10;
-      div {
+      .control {
         display: flex;
         justify-content: center;
         align-items: center;
         width: 1.5rem;
         height: 1.5rem;
+        border: 1px solid var(--secondary-clr);
+
+        &.active {
+          background-color: rgba($color: purple, $alpha: 0.5);
+        }
 
         &:not(:last-of-type) {
-          border-right: 1px solid var(--secondary-clr);
+          border-right: none;
+        }
+
+        &:first-of-type {
+          border-top-left-radius: 0.4rem;
+          border-bottom-left-radius: 0.4rem;
+        }
+        &:last-of-type {
+          border-top-right-radius: 0.4rem;
+          border-bottom-right-radius: 0.4rem;
         }
       }
     }
