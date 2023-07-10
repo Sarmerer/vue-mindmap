83c175cd27bb23b404348ec3c11bf348194589fa
sarmerer <sarmerer@gmail.com>
2023-07-10 05:58:49 +0400

    added and improved base components

--- src/components/base/Button.vue
+++ src/components/base/Button.vue
@@ -1,16 +1,100 @@
 <template>
-  <button class="base-button" v-bind="$attrs" v-on="$listeners">
-    <slot></slot>
+  <button
+    :class="['base-button', `base-button--${intent}`]"
+    v-bind="$attrs"
+    v-on="$listeners"
+  >
+    <slot name="icon-left">
+      <BaseIcon v-if="iconLeft" :icon="iconLeft" />
+    </slot>
+
+    <slot />
+
+    <slot name="icon-right">
+      <BaseIcon v-if="iconRight" :icon="iconRight" />
+    </slot>
   </button>
 </template>
 
+<script>
+export default {
+  props: {
+    iconLeft: {
+      type: String,
+      default: "",
+    },
+
+    iconRight: {
+      type: String,
+      default: "",
+    },
+
+    intent: {
+      type: String,
+      default: "primary",
+      validator: (value) =>
+        ["primary", "secondary", "danger", "text"].includes(value),
+    },
+  },
+};
+</script>
+
 <style lang="scss" scoped>
 .base-button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 4px;
+
+  transition: background-color 0.3s;
   cursor: pointer;
   margin: 0;
   outline: none;
   border: none;
+  border-radius: 4px;
   background: none;
-  padding: 0;
+  padding: 8px 16px;
+  font-size: 14px;
+}
+
+.base-button:disabled {
+  opacity: 0.5;
+  cursor: not-allowed;
+}
+
+.base-button--primary {
+  background-color: var(--color-main-foreground);
+  color: var(--color-main-background);
+}
+
+.base-button--primary:hover:enabled {
+  background-color: var(--color-main-foreground-hover);
+}
+
+.base-button--secondary {
+  background-color: var(--color-main-background);
+  color: var(--color-main-foreground);
+}
+
+.base-button--secondary:hover:enabled {
+  background-color: var(--color-main-background-hover);
+}
+
+.base-button--danger {
+  background-color: var(--color-danger-foreground);
+  color: var(--color-danger-background);
+}
+
+.base-button--danger:hover:enabled {
+  background-color: var(--color-danger-foreground-hover);
+}
+
+.base-button--text {
+  background-color: transparent;
+  color: var(--color-main-foreground);
+}
+
+.base-button--text:hover:enabled {
+  background-color: var(--color-main-background-hover);
 }
 </style>
---
+++ src/components/base/Textarea.vue
@@ -0,0 +1,63 @@
+<template>
+  <textarea
+    class="base-textarea"
+    :value="value_"
+    :placeholder="placeholder"
+    @input="$emit('input', $event.target.value)"
+    @keydown.prevent.stop.enter.exact="$emit('enter', $event)"
+    @keydown.escape="$emit('focusout', $event)"
+    @keydown.enter.shift.exact.prevent="value_ += '\n'"
+    @focusout="$emit('focusout', $event)"
+  ></textarea>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      default: "",
+    },
+
+    placeholder: {
+      type: String,
+      default: "",
+    },
+
+    autofocus: {
+      type: [Boolean, Number],
+      default: false,
+    },
+  },
+
+  data() {
+    return {
+      value_: this.value,
+    };
+  },
+
+  mounted() {
+    this.maybeFocus();
+  },
+
+  methods: {
+    focus() {
+      this.$el.focus();
+    },
+
+    maybeFocus() {
+      if (typeof this.autofocus === "number") {
+        setTimeout(() => this.focus(), this.autofocus);
+      } else if (this.autofocus) {
+        this.focus();
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.base-textarea {
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+</style>
